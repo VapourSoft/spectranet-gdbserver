d9aade7ae0af2992afa3da0a58a5d26d3919fcee
AvNavSim <shane_comins@hotmail.com>
2025-08-14 20:14:35 +0100

    comment

--- src/rsx_cfunc.c
+++ src/rsx_cfunc.c
@@ -1,22 +1,41 @@
-// Minimal C function for RSX linkage test
-void rsx_c_banner(void)  {
+
+// Minimal C function for RSX linkage test - NOTE cant use library functions !!!!
+
+
+// With __z88dk_fastcall, the first argument is passed in the HL register (not on the stack),
+// as this is not __naked function, the C compiler will generate a prologue and epilogue
+// to save and restore registers, so we can use the stack and other registers as needed.    
+
+void printS(const char* str) __z88dk_fastcall 
+{
+    str; // suppress unused warning
     __asm
-
         EXTERN NEXT 
-
         PRINT_STRING equ 9
 
+        ld  de,hl   ; DE = str        
+
+        mvi c,PRINT_STRING
+        ;lxi d,str
+        call NEXT
+        
+        ;msg: defb "C FUMCTION body$"
+    __endasm;
+
+};
+
+
+void rsx_c_banner(void)  {
+/*    __asm
+        EXTERN NEXT 
+        PRINT_STRING equ 9
         mvi c,PRINT_STRING
         lxi d,msg
         call NEXT
-
-;        ld e, 'C'
-;        ld c, 2
-;        call 5
-        
         ret
-
         msg: defb "C FUMCTION body$"
+    __endasm;*/
 
-        __endasm;
+    printS("\nRSX C function body\n$");
+       
 }
